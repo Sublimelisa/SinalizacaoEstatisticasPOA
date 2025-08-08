9f1bd4e547450b366a01db9f4f09df519512929c
Elisa A. <elisasocial@icloud.com>
2025-08-08 00:17:43 +0000

    Versão 1

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 # Abrir biblioteca e organizar dados
 f = open("sinalizacao.csv")
 
+
+antiga = ''
+primeira = True
+ausentes = []
 for line in f:
-    print(line.strip().split(';'))
+  if primeira:
+    primeira = False
+    continue
+  lista = line.strip().split(';')
+  if antiga == '' or antiga > lista[4]:
+    antiga = lista[4]
+  if len(lista) > 14 and (lista[13].strip() == '' or lista[14].strip() == ''):
+      # Your logic here
+    ausentes.append(lista)
+  else:
+    print(lista)
+    
+# Mostrar dados
+
+print(antiga)
+print(ausentes)
 
 # Repositório criado para armazenar o código
+
